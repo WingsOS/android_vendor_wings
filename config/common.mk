389936d3f3ca5cc147076fe72b9de3cb4e39612a
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-18 19:01:11 -0300

    vendor: Import bootanimation from pixelstyle
    Change-Id: I54dfc3cf5c27a4aa10dac62892329cd25af99228

--- config/common.mk
+++ config/common.mk
@@ -55,6 +55,9 @@ PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 # leave less information available via JDWP.
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 
+# Bootanimation
+include vendor/wings/config/bootanimation.mk
+
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
