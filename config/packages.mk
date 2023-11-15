0d32c06291412e82d43ebd4ae49b3eb0aa6413f0
ponces <ponces26@gmail.com>
2023-11-15 16:02:38 +0000

    vendor: Add AOSPA's face unlock implementation
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -31,6 +31,16 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/bin/getcap \
     system/bin/setcap
 
+# Face Unlock
+PRODUCT_PACKAGES += \
+    FaceUnlock
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+
 # Filesystems tools
 PRODUCT_PACKAGES += \
     fsck.ntfs \
