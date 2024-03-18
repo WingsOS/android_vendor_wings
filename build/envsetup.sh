cb3386a9a408ddb22103a46d791aa6c581b901f4
Stella Bloom <windowz414@gnuweeb.org>
2024-03-18 10:10:16 +0100

    vendor: envestup: Always use `python3` when executing `repopick`
    * Otherwise we get "permission denied" as the file doesn't have the
      executable bit set.
    
    Signed-off-by: Stella Bloom <windowz414@gnuweeb.org>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -934,7 +934,7 @@ alias cmkap='dopush cmka'
 
 function repopick() {
     T=$(gettop)
-    $T/vendor/wings/build/tools/repopick.py $@
+    python3 $T/vendor/wings/build/tools/repopick.py $@
 }
 
 function sort-blobs-list() {
