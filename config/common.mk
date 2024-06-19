bananadroid/android_vendor_banana

vendor: Add preinstalled package list to stop auto cloning
Only extra adds needed, gms now ships it's own lists for this.

config/common.mk
@@ -67,6 +67,10 @@ PRODUCT_COPY_FILES += \
     vendor/banana/prebuilt/common/etc/permissions/privapp-permissions-livedisplay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-livedisplay.xml \
     vendor/banana/prebuilt/common/etc/permissions/privapp-permissions-custom.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-custom.xml
 
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/banana/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-custom-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-custom-product.xml
+
 # Include Banana extra packages
 include vendor/banana/config/packages.mk
 include vendor/banana/config/prop.mk

WingsOS/android_vendor_wings

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Change-Id: I3a8cc48bba0db01a3b0b8ea497ef456f644b43a0

config/common.mk
@@ -109,6 +109,10 @@ PRODUCT_PACKAGES += \
     product_charger_res_images \
     product_charger_res_images_vendor
 
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/wings/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-wings-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-wings-product.xml
+
 # Protobuf - Workaround for prebuilt Qualcomm HAL
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-3.9.1-vendorcompat \