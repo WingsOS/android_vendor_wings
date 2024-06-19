Revolved-UI/avendor_lineage

vendor: Import Audio's from rising-oss
* Update default ringtone, notification and alarm tone

Signed-off-by: SharmagRit <sharmagritstuff@gmail.com>

audio/audio.mk
@@ -1,0 +1,33 @@
+#
+# Copyright (C) 2023 The risingOS Android Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/lineage/audio
+
+AUDIO_SOURCE_DIR := vendor/lineage/audio
+AUDIO_DEST_DIR := $(TARGET_COPY_OUT_PRODUCT)/media/audio
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(AUDIO_SOURCE_DIR)/alarms,$(AUDIO_DEST_DIR)/alarms) \
+    $(call find-copy-subdir-files,*,$(AUDIO_SOURCE_DIR)/notifications,$(AUDIO_DEST_DIR)/notifications) \
+    $(call find-copy-subdir-files,*,$(AUDIO_SOURCE_DIR)/ringtones,$(AUDIO_DEST_DIR)/ringtones) \
+    $(call find-copy-subdir-files,*,$(AUDIO_SOURCE_DIR)/ui,$(AUDIO_DEST_DIR)/ui)
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.config.ringtone=23_echo_of_fate.ogg \
+    ro.config.notification_sound=01_oi!.ogg \
+    ro.config.alarm_alert=11_isekai.ogg
+

WingsOS/android_vendor_wings

audio/audio.mk
@@ -1,0 +1,32 @@
+#
+# Copyright (C) 2023 The risingOS Android Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/wings/audio
+
+AUDIO_SOURCE_DIR := vendor/everest/audio
+AUDIO_DEST_DIR := $(TARGET_COPY_OUT_PRODUCT)/media/audio
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(AUDIO_SOURCE_DIR)/alarms,$(AUDIO_DEST_DIR)/alarms) \
+    $(call find-copy-subdir-files,*,$(AUDIO_SOURCE_DIR)/notifications,$(AUDIO_DEST_DIR)/notifications) \
+    $(call find-copy-subdir-files,*,$(AUDIO_SOURCE_DIR)/ringtones,$(AUDIO_DEST_DIR)/ringtones) \
+    $(call find-copy-subdir-files,*,$(AUDIO_SOURCE_DIR)/ui,$(AUDIO_DEST_DIR)/ui)
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.config.ringtone=23_echo_of_fate.ogg \
+    ro.config.notification_sound=01_oi!.ogg \
+    ro.config.alarm_alert=11_isekai.ogg